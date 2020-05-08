00a7230982590af87d3cb3ae0ccc7f9d90554a35
RenatiusvanSander / R. Meier <remad@web.de>
2020-05-08 11:41:01 +0200

    components-and-layouts: main route added

--- src/main/java/com/vaadin/tutorial/crm/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/MainView.java
@@ -1,7 +1,9 @@
 package com.vaadin.tutorial.crm;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
 
+@Route("")
 public class MainView extends VerticalLayout {
 
     public MainView() {
