301dd1c00f638a894739d429ce0fb798ca949b34
RenatiusvanSander / R. Meier <remad@web.de>
2020-05-10 21:15:41 +0200

    Setup-JPA-and-database: jpa and h2 db are running

--- src/main/java/com/vaadin/tutorial/crm/backend/repository/CompanyRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/backend/repository/CompanyRepository.java
@@ -1,4 +1,7 @@
 package com.vaadin.tutorial.crm.backend.repository;
 
-public interface CompanyRepository {
+import com.vaadin.tutorial.crm.backend.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
 }
