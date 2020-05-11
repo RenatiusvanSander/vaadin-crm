cb911ef495ddae29bd50f634dedd49f6ce7e9146
RenatiusvanSander / R. Meier <remad@web.de>
2020-05-11 09:26:56 +0200

    add-data-columns: designed UI grid

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -1,7 +1,9 @@
 package com.vaadin.tutorial.crm.ui;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.tutorial.crm.backend.entity.Contact;
 
 /**
  * This handles the main router to localhost:8080 and its layout.
@@ -9,7 +11,13 @@ import com.vaadin.flow.router.Route;
 @Route("")
 public class MainView extends VerticalLayout {
 
+    Grid<Contact> grid = new Grid<>(Contact.class);
+
     public MainView() {
+        addClassName("list-view");
+        setSizeFull();
+
+        add(grid);
     }
 
 }
