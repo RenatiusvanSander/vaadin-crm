8be74143d5f1c78ad894dd0dd7e13249f835df7c
RenatiusvanSander / R. Meier <remad@web.de>
2020-05-11 10:16:28 +0200

    add-data-columns: data loaded into grid

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -4,6 +4,8 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.tutorial.crm.backend.entity.Contact;
+import com.vaadin.tutorial.crm.backend.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * This handles the main router to localhost:8080 and its layout.
@@ -11,13 +13,29 @@ import com.vaadin.tutorial.crm.backend.entity.Contact;
 @Route("")
 public class MainView extends VerticalLayout {
 
+
     Grid<Contact> grid = new Grid<>(Contact.class);
 
-    public MainView() {
+    public MainView(@Autowired ContactService contactService) {
+        this.contactService = contactService;
+
         addClassName("list-view");
         setSizeFull();
+        configureGrid();
 
         add(grid);
+        updateList();
     }
 
+    private void updateList() {
+        grid.setItems(contactService.findAll());
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact.grid");
+        grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email", "status", "company");
+    }
+
+    private final ContactService contactService;
 }
