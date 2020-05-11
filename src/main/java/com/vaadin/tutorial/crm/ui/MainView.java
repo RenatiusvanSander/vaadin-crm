b7a300f9a42ad5760e989e09852d114bc858d670
RenatiusvanSander / R. Meier <remad@web.de>
2020-05-11 09:22:41 +0200

    add-data-columns: initial branch commit

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -3,6 +3,9 @@ package com.vaadin.tutorial.crm.ui;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
+/**
+ * This handles the main router to localhost:8080 and its layout.
+ */
 @Route("")
 public class MainView extends VerticalLayout {
 
